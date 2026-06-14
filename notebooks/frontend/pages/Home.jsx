33c8ff603962fd4f8ed9987af6da2f3ade2b36a2
Papayaykware <javierciborrogranados@protonmail.com>
2026-06-14 19:23:57 +0100

    Create Home component with ChatWindow

---
+++ notebooks/frontend/pages/Home.jsx
@@ -0,0 +1,10 @@
+import ChatWindow from "../components/ChatWindow";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Agente Educativo Psicocognitivo</h2>
+      <ChatWindow />
+    </div>
+  );
+}
